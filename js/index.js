b992f3af61f0516094a777ef99c0b6b0e5cf43e6
Timothy Sharkey <Techne3@gmail.com>
2019-09-02 18:33:58 -0600

    JSON update

--- js/index.js
+++ js/index.js
@@ -39,4 +39,87 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent['nav']["img-src"])
+
+let code = document.getElementById("cta-img");
+code.setAttribute('src',siteContent['cta']['img-src'])
+
+let middle = document.getElementById('middle-img');
+middle.setAttribute('src',siteContent['main-content']['middle-img-src'])
+
+
+// Navigation ////////
+let navItems = document.querySelectorAll('a');
+
+navItems[0].innerText=siteContent['nav']['nav-item-1']
+navItems[1].innerText=siteContent['nav']['nav-item-2']
+navItems[2].innerText=siteContent['nav']['nav-item-3']
+navItems[3].innerText=siteContent['nav']['nav-item-4']
+navItems[4].innerText=siteContent['nav']['nav-item-5']
+
+
+const newATag1 = document.createElement('a')
+const mainNav = document.querySelector('nav')
+newATag1.innerText = 'Reviews'
+newATag1.href = '#'
+mainNav.prepend(newATag1)
+newATag1.style.color = 'green'
+
+
+
+ const newATag2 = document.createElement('a')
+const mainNav2 = document.querySelector('nav')
+newATag2.innerText = 'Info'
+newATag2.href = '#'
+mainNav2.append(newATag2)
+newATag2.style.color = 'green'
+
+
+// CTA////////////
+
+let ctaHead = document.querySelector('h1');
+ctaHead.innerText = siteContent['cta']['h1']
+
+let ctaButton = document.querySelector('button');
+ctaButton.textContent = siteContent['cta']['button']
+
+// //Main////////////////
+
+// let mainH4 =document.querySelector('text-content h4')
+// mainH4.innerText = siteContent['main-content']['features-h4']
+
+
+// let ctaContent = document.querySelector('p')
+// ctaContent.textContent=siteContent['main-content']['features-content']
+
+
+// let aboutH4 =document.querySelector('h4')
+// aboutH4.innerText = siteContent['main-content']['about-h4']
+
+// let ctaContents = document.querySelector('p')
+// ctaContents.innerText=siteContent['main-content']['about-content']
+
+
+let mainContent = document.querySelectorAll('.text-content')
+
+ mainContent[0].querySelector('h4').textContent = siteContent['main-content']['features-h4']
+mainContent[0].querySelector('p').textContent = siteContent['main-content']['features-content']
+mainContent[1].querySelector('h4').textContent = siteContent['main-content']['about-h4']
+mainContent[1].querySelector('p').textContent = siteContent['main-content']['about-content']
+mainContent[2].querySelector('h4').textContent = siteContent['main-content']['services-h4']
+mainContent[2].querySelector('p').textContent = siteContent['main-content']['services-content']
+mainContent[3].querySelector('h4').textContent = siteContent['main-content']['product-h4']
+mainContent[3].querySelector('p').textContent = siteContent['main-content']['product-content']
+mainContent[4].querySelector('h4').textContent = siteContent['main-content']['vision-h4']
+mainContent[4].querySelector('p').textContent = siteContent['main-content']['vision-content']
+
+let contactInfo = document.querySelector('.contact')
+
+ contactInfo.getElementsByTagName('h4')[0].textContent = siteContent["contact"]["contact-h4"]
+contactInfo.getElementsByTagName('p')[0].textContent = siteContent["contact"]["address"]
+contactInfo.getElementsByTagName('p')[1].textContent = siteContent["contact"]["phone"]
+contactInfo.getElementsByTagName('p')[2].textContent = siteContent["contact"]["email"]
+
+
+let footer = document.querySelector('footer')
+ footer.innerText = "Copyright Great Idea! 2018" 
